1471f184d8970826bef77d203749531f988760f7
Melody Parker <98864236+melparker101@users.noreply.github.com>
2022-11-07 13:45:07 +0000

    Create trim_adapters.sh

---
+++ trim_adapters.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SECONDS=0
+
+# Specify a job name
+#$ -N trim_adapters
+
+# Project name and target queue choose short or long
+#$ -P lindgren.prjc
+#$ -q short.qe
+
+# Run the job in the current working directory
+#$ -cwd -j y
+
+# Log locations which are relative to the current
+# working directory of the submission
+#$ -o logs/output.log
+#$ -e logs/error.log
+
+# Parallel environemnt settings
+#  For more information on these please see the wiki
+#  Allowed settings:
+#   shmem
+#   mpi
+#   node_mpi
+#   ramdisk
+#$ -pe shmem 4
+#$ -t 1:15
+
+fastq=//well/lindgren/users/mzf347/alignment/ivf_cumulus/raw_reads
+OUT=//well/lindgren/users/mzf347/alignment/ivf_cumulus/trimmed_reads2
+
+# index is a file with list of 8 digit numbers (part of the file name)
+# this means input file is the task_id'th line of that list
+INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' index.txt)
+
+module load Trimmomatic/0.39-Java-11
+
+java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar \
+PE \
+$fastq/IVF00"$INPUT_FILE"_R1_001.fastq.gz \
+$fastq/IVF00"$INPUT_FILE"_R2_001.fastq.gz \
+$OUT/IVF00"$INPUT_FILE"_R1_001_trimmed_P.fastq.gz \
+$OUT/IVF00"$INPUT_FILE"_R1_001_trimmed_U.fastq.gz \
+$OUT/IVF00"$INPUT_FILE"_R2_001_trimmed_P.fastq.gz \
+$OUT/IVF00"$INPUT_FILE"_R2_001_trimmed_U.fastq.gz \
+ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/NexteraPE-PE.fa:2:30:10 \
+MINLEN:20
+
+echo "Adapter removed."
