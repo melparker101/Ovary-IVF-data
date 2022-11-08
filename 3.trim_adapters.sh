919fd01d405e711042bb678f6dde315d0ab701fd
Melody Parker <98864236+melparker101@users.noreply.github.com>
2022-11-08 15:00:23 +0000

    Update 3.trim_adapters.sh

--- 3.trim_adapters.sh
+++ 3.trim_adapters.sh
@@ -37,18 +37,18 @@ OUT=//well/lindgren/users/mzf347/alignment/ivf_cumulus/trimmed_reads2
 
 # index is a file with list of 8 digit numbers (part of the file name)
 # this means input file is the task_id'th line of that list
-INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' fastq/../index.txt)
+INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' fastq/index.txt)
 
 module load Trimmomatic/0.39-Java-11
 
 java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar \
 PE \
-$fastq/IVF00"$INPUT_FILE"_R1_001.fastq.gz \
-$fastq/IVF00"$INPUT_FILE"_R2_001.fastq.gz \
-$OUT/IVF00"$INPUT_FILE"_R1_001_trimmed_P.fastq.gz \
-$OUT/IVF00"$INPUT_FILE"_R1_001_trimmed_U.fastq.gz \
-$OUT/IVF00"$INPUT_FILE"_R2_001_trimmed_P.fastq.gz \
-$OUT/IVF00"$INPUT_FILE"_R2_001_trimmed_U.fastq.gz \
+$fastq/"$INPUT_FILE"_R1_001.fastq.gz \
+$fastq/"$INPUT_FILE"_R2_001.fastq.gz \
+$OUT/"$INPUT_FILE"_R1_001_trimmed_P.fastq.gz \
+$OUT/"$INPUT_FILE"_R1_001_trimmed_U.fastq.gz \
+$OUT/"$INPUT_FILE"_R2_001_trimmed_P.fastq.gz \
+$OUT/"$INPUT_FILE"_R2_001_trimmed_U.fastq.gz \
 ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/NexteraPE-PE.fa:2:30:10 \
 MINLEN:20
 
