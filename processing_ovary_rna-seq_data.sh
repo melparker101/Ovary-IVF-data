2564aa820a5c79b47b3192782aa8a79bf5a8c066
Melody Parker <98864236+melparker101@users.noreply.github.com>
2022-11-11 14:04:46 +0000

    Update processing_ovary_rna-seq_data.sh

--- processing_ovary_rna-seq_data.sh
+++ processing_ovary_rna-seq_data.sh
@@ -3,7 +3,10 @@
 #############################################################
 ## Processing Lindgren In-House Ovary Data
 ## melodyjparker14@gmail.com - Nov 22
-## This script takes maps fastq reads to the hg38 108 reference genome. 
+## This script processes our in-house, ovary, RNA-seq data.
+## It inputs raw reads and a reference genome.
+## It outputs a count matrix.
+## It uses the hg38 108 reference genome.
 ## Based on a workflow provided by Saskia Reibe.
 #############################################################
 
@@ -13,7 +16,8 @@
 # - make index file for raw fastq file names
 # - make STAR genome index
 # - make RSEM transcript ref
-# - run quality_control.sh
+# - perform quality control
+# - run trim_reads.sh
 # - use multiqc on fastqc results
 # - run mapping_and_quantification.sh
 # - generate count matrix
@@ -51,50 +55,49 @@ echo "Started at: "`date`
 echo "------------------------------------------------"
 
 ###################################
-# 2 - DEFINE PATHS AND FILE NAMES
+# 2 - CONSTANTS
 ###################################
-# Paths:
-FASTQ=$1  # Path of the directory containing the raw reads
-REF_GENOME=$2  # Path of the directory containing the reference genome filesref_genome
-STAR_INDEX=star_index
-RSEM=rsem
-
-# FASTQ=//well/lindgren/users/mzf347/alignment/ivf_cumulus/raw_reads
-# REF_GENOME=//well/lindgren/users/mzf347/ref_genomes/homo_sapiens/gencode/GRCh38.p13
-# STAR_INDEX=//well/lindgren/users/mzf347/alignment/ivf_cumulus/star_index   
-# RSEM=//well/lindgren/users/mzf347/alignment/ivf_cumulus/rsem
-
-# File names:
+# Inputs:
+RAW_READS=$1  # Path of the directory containing the raw reads
+REF_GENOME=$2  # Path of the directory containing the reference genome files
 ref_genome_fasta=GRCh38.primary_assembly.genome.fa
 ref_genome_gtf=gencode.v42.primary_assembly.annotation.gtf
+
+# Output directory names:
+TRIMMED_READS=trimmed_reads
+STAR_INDEX=star_index
+STAR_OUT=star
+RSEM_REF=rsem_ref
+RSEM_OUT=rsem
+QC=qc_results
+
 
 ###################################
 # 3 - LOAD MODULES
 ###################################
 module load STAR/2.7.9a-GCC-11.2.0
 module load RSEM/1.3.2-foss-2018b
+module load FastQC/0.11.9-Java-11
 module load MultiQC/1.9-foss-2019b-Python-3.7.4
 
 ###################################
 # 4 - START MAIN CODE
 ###################################
+# Have script stop if there is an error
+set -e
+
 # Make an index for the raw reads file names
-if [ ! -f $FASTQ/index.txt ]
+if [ ! -f $RAW_READS/index.txt ]
 then
-  for f in $fastq/IVF*R1*; do g="${f%_R1*}" ; echo ${g##*/} >> $FASTQ/index.txt ; done
+  for f in $RAW_READS/IVF*R1*; do g="${f%_R1*}" ; echo ${g##*/} >> $RAW_READS/index.txt ; done
   echo "Index file created."
-else
-  echo "Index file already exists."
 fi
 
 
 # Make a genome index using STAR
-if [ ! -d "$STAR_INDEX" ]
+if [ ! -p "$STAR_INDEX" ]
 then
   mkdir -p $STAR_INDEX
-  echo "STAR index directory created."
-else
-  echo "STAR index directory already exists."
 fi
 
 STAR --runThreadN 6 \
@@ -105,29 +108,44 @@ STAR --runThreadN 6 \
 --sjdbOverhang 149  # read-length - 1
 
 # Build a transcript reference using RSEM.
-
-if [ ! -d $RSEM_REF ]
+if [ ! -p $RSEM_REF ]
 then
   mkdir -p $RSEM_REF
-  echo "RSEM reference directory created."
 fi
-  echo "RSEM reference directory already exists."
 
 rsem-prepare-reference --gtf $REF_GENOME/ref_genome_gtf \
    				     $REF_GENOME/ref_genome_fasta $RSEM_REF/human
               
-# The next steps will be run in parallel so require a separate script.
 
-# Run this script to perform QC, trim adapters, run QC again, if required.
-sh quality_control.sh
-multiqc qc_raw_results -o qc_raw_results
-multiqc qc_trimmed_results -o qc_trimmed_results
+# Perform QC on raw reads
+mkdir $QC/qc_raw_results
+fastqc $RAW_READS/*.fq -o $QC/qc_raw_results
+multiqc $QC/qc_raw_results -o $QC/qc_raw_results
+echo "QC on raw reads is finished."
+
+
+# Trim reads
+# This will be run in parallel so requires a separate script
+if [ ! -p $TRIMMED_READS ]
+then
+  sh trim_reads.sh $RAW_READS $TRIMMED_READS
+fi
+
+
+# Perform QC on trimmed reads
+mkdir $QC/qc_trimmed_results
+fastqc $TRIMMED_READS/*.fq -o $QC/qc_trimmed_results
+multiqc $QC/qc_trimmed_results -o $QC/qc_trimmed_results
+echo "QC on trimmed reads is finished."
+
 
 # map reads to reference genome, then quantify genes and isoforms.
-sh mapping_and_quantification.sh
+sh mapping_and_quantification.sh -raw $RAW_READS -trimmed $TRIMMED_READS -ref $REF_GENOME \
+                                 -star_index $STAR_INDEX -star $STAR_OUT \
+                                 -rsem_ref $RSEM_REF -rsem $RSEM_OUT
 
 # Generate a count matrix from the quantification results.
-rsem-generate-data-matrix $RSEM/*.genes.results >> full.count.genes.matrix.txt
+rsem-generate-data-matrix $RSEM_OUT/*.genes.results >> full.count.genes.matrix.txt
 
 # Run downstream analysis
-sh downstream_analysis.r
+# sh downstream_analysis.r
