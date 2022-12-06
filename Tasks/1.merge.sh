933eb62b6ca9809818d311b426c3799898864ced
Melody Parker <98864236+melparker101@users.noreply.github.com>
2022-12-06 17:33:50 +0000

    Update 1.merge.sh

--- Tasks/1.merge.sh
+++ Tasks/1.merge.sh
@@ -45,7 +45,7 @@ IN2=fastq_lane2
 OUT=merged_fastq
 
 
-INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' $fastq/merge_index)
+INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' $IN/index.txt)
 
 
 cat $IN1/IVF_123456_"$INPUT_FILE"_1.fastq.gz $IN2/IVF_987654_"$INPUT_FILE"_1.fastq.gz > $OUT/"$INPUT_FILE"_R1.fastq.gz
