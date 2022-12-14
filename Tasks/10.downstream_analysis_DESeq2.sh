c742f2dd5ea1ca3f64dfc0a3ddec3c5ddcc62f18
Melody Parker <98864236+melparker101@users.noreply.github.com>
2022-12-14 17:53:05 +0000

    Update 10.downstream_analysis_DESeq2.sh

--- Tasks/10.downstream_analysis_DESeq2.sh
+++ Tasks/10.downstream_analysis_DESeq2.sh
@@ -6,20 +6,20 @@
 ## http://master.bioconductor.org/packages/release/workflows/vignettes/rnaseqGene/inst/doc/rnaseqGene.html
 #############################################################
 
-##############################
-# 0 - Workflow
-##############################
+# Still need to update count matrix to include IVF0014!
+
+##############################################
+# Workflow
+##############################################
 ## - Create a DESeqDataSet object
 ## - Perform exploratory analysis and visualization
 ## - Perform differential expression analysis
 
-##############################
-# 1 - Load librairies
-##############################
+##############################################
+# Load librairies
+##############################################
 # library(tidyverse)
 # library("AnnotationDbi")
-# library("org.Hs.eg.db")
-# library(geneplotter)
 # library(tidyverse)
 # library(ggbiplot)
 # library(gplots)
@@ -32,34 +32,46 @@ library("pheatmap")  # For the heatmap
 library("RColorBrewer")  # For the heatmap colours
 library("PoiClaClu")  # For poisson distance
 library(tidyverse) 
+library(geneplotter)  # For multiple plots on top of each other, multidensity()
+library("org.Hs.eg.db")  # For adding symbol, entrez, and uniprot columns
 
-############################## 
-# 2 - Set working directory
-##############################
+##############################################
+# Source files
+##############################################
 # setwd("ivf_cumulus")
-dir()
+# dir()
 
-############################## 
-# 3 - Source file
-##############################
-# input_file <- "full.count.genes.matrix.txt"
 in_counts_file <- "count_matrix.txt"  # Counts should NOT be normalised
-in_phenotype_file <- "IVF_phenotypic_data_repox_15nov2022_trimmed.csv"
+in_phenotype_file <- "pheno_data_revised.csv"
 out_norm_counts <- "normalized_counts.txt"
 
-############################## 
-# 4 - Start Code
-##############################
-### CREATE DESEQDATASET OBJECT ###
+##############################################
+# Functions
+##############################################
+# Save as png image 
+SaveAsPDF <- function(plot_name,plot) {
+png_name <- paste("plots/", plot_name, ".pdf", sep="")
+pdf(file=png_name,
+width=20)
 
-# Read in data
+plot
+
+# Save the file
+dev.off()
+}
+
+
+##############################################
+# 1. Create DESeq Dataset Object
+##############################################
+# Read in count data and phenotype data
 count_data <- read.delim(in_counts_file, header = T, sep="\t", row.names = 1,stringsAsFactors=FALSE)
 pheno_data <- read.csv(in_phenotype_file)
 
+# coldata: samples, fert_cat
+# also try coldata= samples,fert_cat,
 
-samples <- c()
-condition <- c()
-
+# Samples
 # Trim column names to match the IDs in the phenotype data
 samples <- colnames(count_data)
 samples <- gsub("^.{0,5}", "", samples)  # Removes the first 5 chars
@@ -67,35 +79,44 @@ samples <- gsub("\\..*","",samples)  # Removes the end after .
 samples <- gsub(".*_","",samples)  # Removed the start up to _
 colnames(count_data) <- samples
 
+# Phenotype data
 pheno_data <- subset(pheno_data, ID %in% samples)
 dim(pheno_data)
-
 # Check that they match
 all(colnames(count_data) %in% pheno_data$ID)
 all(colnames(count_data) == pheno_data$ID)
 # Use match() to rearrange if needed
 
-# View just CaseControl and ReasonforIVF columns
-pheno_data[,c("ID","CaseControl","ReasonForIVF")]
+# Get rid of columns with all NA
+# pheno_data[,colSums(is.na(pheno_data))<nrow(pheno_data)]
+
+# Decide which columns to use
+cols <- c("ID","SurgAge","CycleDay","No.of.follicles..18mm","Antral.Follicles","Weight..kg.","Height..cm.","ICSI.IVF","Proposed_categories","BMI")
+all(cols %in% colnames(pheno_data))
+coldata <- pheno_data[,cols]
+
+# Clean up coldata
+rownames(coldata) <- NULL
+coldata$ICSI.IVF <- str_trim(coldata$ICSI.IVF)
+# class(BMI)
+str(coldata)
+
+# BMI and Antral.Follicles are both chr - change them to int
+coldata$BMI <- str_remove(coldata$BMI, "BMI ")
+coldata$Antral.Follicles <- as.integer(coldata$Antral.Follicles)
+coldata$BMI <- as.integer(coldata$BMI)
+str(coldata)
+
+keep <- rowSums(counts(ddsMat)) > 0
+# View(counts(ddsMat[!keep,]))
+ddsMat <- ddsMat[keep,]
+
+# View just CaseControl, ReasonforIVF and Proposed_categories columns
+pheno_data[,c("ID","ReasonForIVF","Proposed_categories")]
 
 # Group male factor together
-pheno_data$ReasonForIVF[grep("Male|Sperm",pheno_data$ReasonForIVF)]<-"Male Factor"
-
-
-IVF_reason <- pheno_data$ReasonForIVF
-sample_type <- pheno_data$sample
-age <- pheno_data$SurgAge
-PCOS <- pheno_data$CaseControl
-cycle_day <- pheno_data$CycleDay
-folicles_18mm <- pheno_data$No.of.follicles..18mm
-antral_folicles <- pheno_data$Antral.Follicles
-AHM <- pheno_data$AMH.levels
-BMI <- pheno_data$BMI
-BMI <- str_remove(BMI, "BMI ")
-ICSI_IVF <- pheno$ICSI.IVF
-
-################# change condition to IVF_reason
-coldata <- as.data.frame(cbind(samples,IVF_reason))
+# Redundant now that we have propose categories
+# pheno_data$ReasonForIVF[grep("Male|Sperm",pheno_data$ReasonForIVF)]<-"Male Factor"
 
 # Create a matrix
 count_mat <- as.matrix(count_data)
@@ -110,7 +131,7 @@ countdata <- na.omit(count_mat)
 # Create a DESeqDataSet object
 ddsMat <- DESeqDataSetFromMatrix(countData = count_mat,
                                  colData = coldata,
-                                 design = ~IVF_reason)
+                                 design = ~Proposed_categories)
                                  
 head(assay(ddsMat))  
 
@@ -120,6 +141,7 @@ View(counts(ddsMat))
 # Filter out empty rows
 # Strict filtering to increase power is automatically applied later
 keep <- rowSums(counts(ddsMat)) > 0
+# View(counts(ddsMat[!keep,]))
 ddsMat <- ddsMat[keep,]
 
 # We can also filter like this
@@ -129,7 +151,6 @@ ddsMat <- ddsMat[keep,]
 # Add control features for estimating size factors
 # ddsMat <- estimateSizeFactors(ddsMat, controlGenes=ctrlGenes)
 ddsMat <- estimateSizeFactors(ddsMat)
-ddsMat <- DESeq(ddsMat)
 
 # View normalisation factor applied and compare non-normalised data with normalised data
 sizeFactors(ddsMat)
@@ -146,12 +167,13 @@ idx.nz <- apply(GeneCounts, 1, function(x) { all(x > 0)})
 sum(idx.nz)
 
 # Plots
-# These didn't work
-# multidensity( counts(ddsMat, normalized = T)[idx.nz ,],    
-#							xlab="mean counts", xlim=c(0, 1000),main="Normalised data")
-# multidensity( counts(ddsMat, normalized = F)[idx.nz ,],         
-#							xlab="mean counts", xlim=c(0, 1000),main="Not normalised data")
+multidensity( counts(ddsMat, normalized = T)[idx.nz ,],    
+ 						xlab="mean counts", xlim=c(0, 1000),main="Normalised data")
+            
+multidensity( counts(ddsMat, normalized = F)[idx.nz ,],         
+						xlab="mean counts", xlim=c(0, 1000),main="Not normalised data")
 
+##############################################
 # Add symbol, entrez, and uniprot columns to the dataframe
 count_data$symbol <- mapIds(org.Hs.eg.db,
                             keys=row.names(count_data),                                                      
@@ -171,8 +193,9 @@ count_data$uniprot <- mapIds(org.Hs.eg.db,
                              keytype="ENSEMBL",
                              multiVals="first")    
 
-### PERFORM EXPLORATORY ANALYSIS AND VISUALISATION ###
-
+##############################################
+# 2. Perform Exploratory Analysis and Visualisation
+##############################################
 # Variance stabilizing transformation
 vsd <- vst(ddsMat, blind = FALSE)
 head(assay(vsd), 3)
@@ -207,7 +230,7 @@ sampleDists
 
 # Visualize the distances in a heatmap
 sampleDistMatrix <- as.matrix( sampleDists )
-rownames(sampleDistMatrix) <- paste( rld$IVF_reason, rld$samples, sep = " - " )
+rownames(sampleDistMatrix) <- paste( rld$fert_cat, rld$samples, sep = " - " )
 colnames(sampleDistMatrix) <- NULL
 colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
 pheatmap(sampleDistMatrix,
@@ -222,32 +245,64 @@ poisd <- PoissonDistance(t(counts(ddsMat)))
 # This measure of dissimilarity between counts also takes the inherent variance structure of counts 
 # into consideration when calculating the distances between samples
 samplePoisDistMatrix <- as.matrix( poisd$dd )
-rownames(samplePoisDistMatrix) <- paste( ddsMat$IVF_reason, ddsMat$samples, sep=" - " )
+rownames(samplePoisDistMatrix) <- paste( ddsMat$fert_cat, ddsMat$samples, sep=" - " )
 colnames(samplePoisDistMatrix) <- NULL
 pheatmap(samplePoisDistMatrix,
          clustering_distance_rows = poisd$dd,
          clustering_distance_cols = poisd$dd,
          col = colors)
 
-# Basic PCA
-plotPCA(rld)
-
 # Annotated PCA
-plotPCA(rld, intgroup = c("IVF_reason", "samples"))
+# I think intgroup just groups by colour and does not affect geometry of the pca plot
+# plotPCA(rld, intgroup = c("Proposed_categories"))
+# plotPCA(rld, intgroup = c("BMI"))
 
-# Make a PCA table from the rld data
-pcaData <- plotPCA(rld, intgroup = c("IVF_reason", "samples"), returnData = TRUE)
+# Make a PCA table from the rld data using argument returnData
+pcaData <- plotPCA(rld, intgroup = cols, returnData = TRUE)
 pcaData
-
 percentVar <- round(100 * attr(pcaData, "percentVar"))
 
 # Make a PCA plot using shapes and colours
-ggplot(pcaData, aes(x = PC1, y = PC2, color = samples, shape = IVF_reason)) +
+ggplot(pcaData, aes(x = PC1, y = PC2, color = BMI, shape = Proposed_categories)) +
   geom_point(size =3) +
   xlab(paste0("PC1: ", percentVar[1], "% variance")) +
   ylab(paste0("PC2: ", percentVar[2], "% variance")) +
   coord_fixed() +
   ggtitle("PCA with rlog data")
+
+# PCA of patient age
+ggplot(pcaData, aes(x = PC1, y = PC2, color = SurgAge)) +
+  geom_point(size =3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  ggtitle("PCA with rlog data")
+
+ggplot(pcaData, aes(x = PC1, y = PC2, color = Antral.Follicles, shape = ICSI.IVF)) +
+  geom_point(size =3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  ggtitle("PCA with rlog data")
+  
+PCA_AF_ICSI <- 
+ggplot(pcaData, aes(x = PC1, y = PC2, color = Antral.Follicles, shape = ICSI.IVF)) +
+  geom_point(size =3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  ggtitle("PCA with rlog data")
+  
+ggsave(filename="plots/PCA_AF_ICSI.pdf", plot=PCA_AF_ICSI, width=8, height=5, units="in")
+  
+# PCA of BMI
+ggplot(pcaData, aes(x = PC1, y = PC2, color = BMI)) +
+  geom_point(size =3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  ggtitle("PCA for BMI with rlog data")
+
 
 # RNA counts, generally, are never normally distributed    
 ggplot(count_data) +
@@ -258,7 +313,7 @@ ggplot(count_data) +
 # GLM-PCA
 gpca <- glmpca(counts(ddsMat), L=2)
 gpca.dat <- gpca$factors
-gpca.dat$condition <- dds$condition
+gpca.dat$fert_cat <- dds$fert_cat
 gpca.dat$samples <- dds$samples
 
 # Plot GLM-PCA
@@ -277,10 +332,15 @@ sampleDists
 mds <- as.data.frame(colData(rld))  %>%
          cbind(cmdscale(sampleDistMatrix))
          
-ggplot(mds, aes(x = `1`, y = `2`, color = samples, shape = condition)) +
+ggplot(mds, aes(x = `1`, y = `2`, color = samples, shape = fert_cat)) +
   geom_point(size = 3) + coord_fixed() + ggtitle("MDS with rld data")
-  
-### DE ANALYSIS ###
+
+##############################################
+# 3. Differential Expression Analysis
+##############################################
+# Run differential expression pipeline
+ddsMat <- DESeq(ddsMat)
+# A DESeqDataSet is returned that contains all the fitted parameters within it
 
 res <- results(ddsMat)
 res
@@ -290,14 +350,14 @@ summary(res)
 res.05 <- results(dds, alpha = 0.05)
 table(res.05$padj < 0.05)
                             
-# Plot counts per condition for top gene
+# Plot counts per fert_cat for top gene
 # Save to pdf
 topGene <- rownames(res)[which.min(res$padj)]
-plotCounts(dds, gene = topGene, intgroup=c("condition"))     
+plotCounts(dds, gene = topGene, intgroup=c("fert_cat"))     
 
 pdf(file="draft_counts_plot.pdf", width=30)
 {par(lwd = 2)
-plotCounts(dds, gene = topGene, intgroup=c("condition"))
+plotCounts(dds, gene = topGene, intgroup=c("fert_cat"))
 }
 dev.off()
 
@@ -311,6 +371,8 @@ with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim
 # Add coloured points: blue if padj<0.01, red if log2FC>1 and padj<0.05)
 with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
 with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+
+
 
 
                                  
