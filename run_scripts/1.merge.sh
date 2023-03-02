edd85fa255baff150d8b3cb507dba6150fbb2744
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-02 11:58:31 +0000

    Update 1.merge.sh

--- run_scripts/1.merge.sh
+++ run_scripts/1.merge.sh
@@ -2,44 +2,37 @@
 
 # ----------------------------------------------------------
 # Script to create merge RNA reads
+# Edit the read names in the script based on the run ids
 # Based on a script from Saskia Reibe
 # melodyjparker14@gmail.com - Dec 22
 # ----------------------------------------------------------
 
-# Specify a job name
-#$ -N merge_RNA_reads
 
-# Project name and target queue choose short or long
-#$ -P lindgren.prjc
-#$ -q short.qe 
+#!/bin/bash
 
-# Run the job in the current working directory
-#$ -cwd -j y
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 4
+#SBATCH -J merge_reads
+#SBATCH -o logs/output.out
+#SBATCH -e logs/error.err
+#SBATCH -a 1-15
 
-# Log locations which are relative to the current
-# working directory of the submission
-###$ -o logs/output.log
-###$ -e logs/error.log
+#  Parallel environment settings 
+#  For more information on these please see the documentation 
+#  Allowed parameters: 
+#   -c, --cpus-per-task 
+#   -N, --nodes 
+#   -n, --ntasks 
 
-# Parallel environemnt settings
-#  For more information on these please see the wiki
-#  Allowed settings:
-#   shmem
-#   mpi
-#   node_mpi
-#   ramdisk
-#$ -pe shmem 4
-#$ -t 1-15
+echo "########################################################"
+echo "Slurm Job ID: $SLURM_JOB_ID" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "##########################################################"
 
-# Some useful data about the job to help with debugging
-echo "------------------------------------------------"
-echo "SGE Job ID: $JOB_ID"
-echo "SGE Task ID: $SGE_TASK_ID"
-echo "Run on host: "`hostname`
-echo "Operating system: "`uname -s`
-echo "Username: "`whoami`
-echo "Started at: "`date`
-echo "------------------------------------------------"
 
 IN=$1  # index_dir
 IN1=$2  # fastq_lane1
@@ -52,7 +45,10 @@ INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' $IN/index.txt)
 
 cat $IN1/IVF_123456_"$INPUT_FILE"_1.fastq.gz $IN2/IVF_987654_"$INPUT_FILE"_1.fastq.gz > $OUT/"$INPUT_FILE"_R1.fastq.gz
 
-cat $IN/IVF_987654_"$INPUT_FILE"_2.fastq.gz $IN2/IVF_987654_"$INPUT_FILE"_2.fastq.gz > $OUT/"$INPUT_FILE"_R2.fastq.gz
+cat $IN/IVF_123456_"$INPUT_FILE"_2.fastq.gz $IN2/IVF_987654_"$INPUT_FILE"_2.fastq.gz > $OUT/"$INPUT_FILE"_R2.fastq.gz
 
 
-# End of job script
+echo "###########################################################"
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
