cb8fa66eeed741456007b9b8442ee4b284f16252
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-02 13:45:41 +0000

    Update and rename Tasks/9.generate count matrix to run_scripts/9.generate_count_matrix.sh

---
+++ run_scripts/9.generate_count_matrix.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Script to run quality check on in-house IVF ovary data (trimmed reads)
+# melodyjparker14@gmail.com - Nov 22
+# ----------------------------------------------------------
+
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 4
+#SBATCH -J generate_count_matrix
+#SBATCH -o logs/output.out
+#SBATCH -e logs/error.err
+
+#  Parallel environment settings 
+#  For more information on these please see the documentation 
+#  Allowed parameters: 
+#   -c, --cpus-per-task 
+#   -N, --nodes 
+#   -n, --ntasks 
+
+echo "########################################################"
+echo "Slurm Job ID: $SLURM_JOB_ID" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "##########################################################"
+
+
+module load RSEM/1.3.2-foss-2018b
+rsem-generate-data-matrix *.genes.results >> full.count.genes.matrix.txt
+
+# Trim the version number off the ensembl IDs
+sed 's/\(ENSG[0-9]*\)\.[0-9]*/\1/g' full.count.genes.matrix.txt > count_matrix.txt
+
+
+echo "###########################################################"
+echo "Count matrix generated."
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
