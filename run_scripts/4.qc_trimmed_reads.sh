df5e3b92dfcc022f22c0f2cff2e84804facf0735
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-02 12:23:51 +0000

    Update 4.qc_trimmed_reads.sh

--- run_scripts/4.qc_trimmed_reads.sh
+++ run_scripts/4.qc_trimmed_reads.sh
@@ -5,43 +5,38 @@
 # melodyjparker14@gmail.com - Nov 22
 # ----------------------------------------------------------
 
-SECONDS=0
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 4
+#SBATCH -J qc_trimmed_reads
+#SBATCH -o logs/output.out
+#SBATCH -e logs/error.err
+#SBATCH -a 1-15
 
-# Specify a job name
-#$ -N qc_trimmed_reads
+#  Parallel environment settings 
+#  For more information on these please see the documentation 
+#  Allowed parameters: 
+#   -c, --cpus-per-task 
+#   -N, --nodes 
+#   -n, --ntasks 
 
-# Project name and target queue choose short or long
-#$ -P lindgren.prjc
-#$ -q short.qe
+echo "########################################################"
+echo "Slurm Job ID: $SLURM_JOB_ID" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "##########################################################"
 
-# Run the job in the current working directory
-#$ -cwd -j y
-
-# Log locations which are relative to the current
-# working directory of the submission
-#$ -o logs/
-#$ -e logs/
-
-# Parallel environemnt settings
-#  For more information on these please see the wiki
-#  Allowed settings:
-#   shmem
-#   mpi
-#   node_mpi
-#   ramdisk
-#$ -pe shmem 4
-#$ -t 1-15
 
 IN=$1  # trimmed_reads
 OUT=$2  # qc_trimmed_results
 
 # Merge index is a file with list of 8 digit numbers (part of the file name)
 # This means input file is the task_id'th line of that list
-INPUT_FILE=$(sed "$SGE_TASK_ID"'q;d' $IN/index.txt)
-
+INPUT_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' $IN/index.txt)
 
 module load FastQC/0.11.9-Java-11
-
 
 echo $INPUT_FILE
 fastqc $IN/"$INPUT_FILE"_R1_001_trimmed_U.fastq.gz -o $OUT
@@ -50,7 +45,8 @@ fastqc $IN/"$INPUT_FILE"_R2_001_trimmed_U.fastq.gz -o $OUT
 fastqc $IN/"$INPUT_FILE"_R2_001_trimmed_P.fastq.gz -o $OUT
 
 
+echo "###########################################################"
 echo "QC finished."
-
-
-# End of script job
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
