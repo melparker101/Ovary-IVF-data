a36cebb261e767857d0578fb89cf24110f3c61c0
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-02 11:46:03 +0000

    Update 0.make_index.sh

--- run_scripts/0.make_index.sh
+++ run_scripts/0.make_index.sh
@@ -5,42 +5,30 @@
 # melodyjparker14@gmail.com - Nov 22
 # ----------------------------------------------------------
 
-# Specify a job name
-#$ -N make_index
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 1
+#SBATCH -J make_index
+#SBATCH -o output.out
+#SBATCH -e error.err
 
-# Project name and target queue choose short or long
-#$ -P lindgren.prjc
-#$ -q short.qe 
+#  Parallel environment settings 
+#  For more information on these please see the documentation 
+#  Allowed parameters: 
+#   -c, --cpus-per-task 
+#   -N, --nodes 
+#   -n, --ntasks 
 
-# Run the job in the current working directory
-#$ -cwd -j y
+echo "########################################################"
+echo "Slurm Job ID: $SLURM_JOB_ID" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "##########################################################"
 
-# Log locations which are relative to the current
-# working directory of the submission
-###$ -o logs/output.log
-###$ -e logs/error.log
 
-# Parallel environemnt settings
-#  For more information on these please see the wiki
-#  Allowed settings:
-#   shmem
-#   mpi
-#   node_mpi
-#   ramdisk
-#$ -pe shmem 1
-
-# Some useful data about the job to help with debugging
-echo "------------------------------------------------"
-echo "SGE Job ID: $JOB_ID"
-echo "SGE Task ID: $SGE_TASK_ID"
-echo "Run on host: "`hostname`
-echo "Operating system: "`uname -s`
-echo "Username: "`whoami`
-echo "Started at: "`date`
-echo "------------------------------------------------"
-
-# Begin writing your script here
-fastq=raw_reads
+fastq=$1  # raw_reads
 
 
 for f in $fastq/IVF*R1*; do g="${f%_R1*}" ; echo ${g##*/} >> $fastq/index.txt ; done
@@ -51,4 +39,8 @@ for f in IVF*R1*.fastq.gz; do echo ${f%_001*} >> index.txt ; done
 fi
 comment
 
-# End of job script
+
+echo "###########################################################"
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
