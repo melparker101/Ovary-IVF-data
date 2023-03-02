084542da6a86ba92fe334b837abe6656f1c1f8b6
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-02 13:31:35 +0000

    Update and rename 6.rna-seq_star.sh to 6.alignment_star.sh

---
+++ Tasks/6.alignment_star.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Script to map in-house IVF ovary RNA-seq data to a reference genome
+# melodyjparker14@gmail.com - Nov 22
+# ----------------------------------------------------------
+
+#SBATCH -A lindgren.prj
+#SBATCH -p short
+#SBATCH -c 4
+#SBATCH -J alignment_star
+#SBATCH -o logs/output.out
+#SBATCH -e logs/error.err
+#SBATCH -a 1-15
+
+#  Parallel environment settings 
+#  For more information on these please see the documentation 
+#  Allowed parameters: 
+#   -c, --cpus-per-task 
+#   -N, --nodes 
+#   -n, --ntasks 
+
+echo "########################################################"
+echo "Slurm Job ID: $SLURM_JOB_ID" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "##########################################################"
+
+
+# module purge 
+# module use -a /apps/eb/dev/{skylake,ivybridge}/modules/all
+module load STAR/2.7.9a-GCC-11.2.0
+
+fastq=$1  # raw_reads
+STAR_INDEX=$2  # star_index
+REF=$3  # ref_genomes/homo_sapiens/gencode/GRCh38.p13
+IN=$4  # trimmed_reads
+OUT=$5  # star
+
+INPUT_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' /index.txt)
+
+STAR --runThreadN 6 \
+--readFilesIn "$IN"/"$INPUT_FILE"_R1_001_trimmed_P.fastq.gz "$IN"/"$INPUT_FILE"_R2_001_trimmed_P.fastq.gz \
+--readFilesCommand zcat \
+--outFileNamePrefix "$OUT"/"$INPUT_FILE"_ \
+--genomeDir $STAR_INDEX \
+--sjdbGTFfile $REF/gencode.v42.primary_assembly.annotation.gtf --outSJfilterReads Unique --sjdbOverhang 149 \
+--outFilterType BySJout --outFilterMultimapNmax 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.1 \
+--alignSJoverhangMin 8 --alignSJDBoverhangMin 3 --alignIntronMin 20 --chimSegmentMin 20 \
+--outSAMtype BAM SortedByCoordinate --outSAMattributes All \
+--quantMode TranscriptomeSAM  # output alignments translated into transcript coordinates
+
+
+echo "###########################################################"
+echo "Read mapping finished."
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
