f36ce2235d5fd9073f1b86dd2c5cf4acc977294c
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-06-08 16:34:32 +0100

    Update 4.trim_adapters.sh

--- run_scripts/4.trim_adapters.sh
+++ run_scripts/4.trim_adapters.sh
@@ -31,10 +31,11 @@ echo "##########################################################"
 
 IN=$1  # raw_reads
 OUT=$2  # trimmed_reads
-
 INPUT_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' IN/index.txt)
 
 module load Trimmomatic/0.39-Java-11
+
+mkdir -p $OUT
 
 java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar \
 PE \
